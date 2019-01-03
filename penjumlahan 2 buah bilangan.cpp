c023cb451fb1eff0e879f270893a8ce5dacaf96a
Dion baharudin <45952796+Dionese@users.noreply.github.com>
2019-01-03 10:19:28 +0700

    Add files via upload

---
+++ penjumlahan 2 buah bilangan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"program fungsi 2 bilangan pertambahan,pembagian,pengurangan,dan perkalian"<<endl;
+    cout<<"masukan nilai x :";cin>>x;
+    cout<<"masukan nilai y :";cin>>y;
+    cout<<"x+y :"<<(x+y)<<endl;
+    cout<<"x-y :"<<(x-y)<<endl;
+    cout<<"x*y :"<<(x*y)<<endl;
+    cout<<"x/y :"<<(x/y)<<endl;
+    return 0;
+}
